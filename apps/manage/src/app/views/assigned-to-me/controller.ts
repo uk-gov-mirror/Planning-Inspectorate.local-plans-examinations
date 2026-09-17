a09721741c76f49740a4e0bbfffbd8d1854a1b0d
Kieran Smith <kieransmith63@gmail.com>
2026-09-17 13:42:44 +0100

    fix: param order

--- apps/manage/src/app/views/assigned-to-me/controller.ts
+++ apps/manage/src/app/views/assigned-to-me/controller.ts
@@ -70,7 +70,7 @@ export function buildAssignedToMe(service: ManageService): AsyncRequestHandler {
 						}
 					});
 
-					const status = resolveCaseHeaderStatus(gateway1Info, gateway2Info, gateway2Documents);
+					const status = resolveCaseHeaderStatus(gateway2Documents, gateway1Info, gateway2Info);
 
 					return {
 						...c,
